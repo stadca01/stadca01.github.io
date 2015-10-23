14946d49a894153f83e267527e85382891180e5c
stadca01 <stadca01@luther.edu>
2015-10-23 13:44:38 -0500

    Update todoapp.js

--- todoapp.js
+++ todoapp.js
@@ -1,3 +1,13 @@
+completeTask = function(){
+  if (this.checked) { 
+  this.parentNode.classList.add("done");
+  }
+  else {
+  this.parentNode.classList.remove("done");
+  }
+  localSave("unlist");
+  }
+
 addTask = function() {
 var litext = document.createTextNode(document.querySelector('#taskbox').value);
 var listitem = document.createElement("li");
@@ -9,22 +19,14 @@ listitem.className = document.querySelector('#priority').value;
 cbox.onclick = completeTask;
 var ulsection1 = document.querySelector('#unlist');
 ulsection1.appendChild(listitem);
+
 localSave("unlist");
 
 
 
 
 }
-completeTask = function(){
-  if (this.checked) { 
-  this.parentNode.classList.add("done");
-  }
-  else {
-  this.parentNode.classList.remove("done");
-  }
-  localSave("unlist");
-  
-}
+
 window.onload = function(){
   restoreList("unlist", completeTask);
 }
